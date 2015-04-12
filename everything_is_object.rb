f4bf96e3e29cfdc4d4dd6f582758d5353b61e01d
Petro Koriakin <pkor@softserveinc.com>
2015-04-12 12:56:12 +0300

    first commit

---
+++ everything_is_object.rb
@@ -0,0 +1,7 @@
+puts "alice".capitalize
+
+puts 5.next
+
+puts false.class
+
+puts [5, 12, 4].sort
---
+++ strings.rb
@@ -0,0 +1,16 @@
+puts "New string"
+
+p String.new
+
+puts "Seconds/day: #{24 * 60 * 60}"
+
+puts 'Seconds/day: #{24 * 60 * 60}'
+
+puts "Tro #{'Lo ' * 3}!!!1"
+
+puts "Now is #{
+            def the(a)
+              'the ' + a
+            end
+            the('time')
+          } for all good coders ..."
